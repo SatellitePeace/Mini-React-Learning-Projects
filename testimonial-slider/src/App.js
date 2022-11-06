48d387ffa4a2ec29d2af6f3de943e13b151196cb
Njoku <njokunneoma1999@gmail.com>
2022-11-06 15:21:08 +0200

    complete react review project

---
+++ testimonial-slider/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Testimonies from "./Testimonies";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Our Testimonials</h1>
+      <Testimonies />
+    </div>
+  );
+}
+
+export default App;
---
+++ testimonial-slider/src/Testimonies.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState } from "react";
+import testimonies from "./data";
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaQuoteRight,
+  FaQuoteLeft,
+} from "react-icons/fa";
+
+export default function Testimonies() {
+  // use states
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = testimonies[index];
+  //   buttons functions
+  // error
+  const checkIndex = (number) => {
+    if (number > testimonies.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return testimonies.length - 1;
+    }
+    return number;
+  };
+  //   nextbtn
+  const nextTestimony = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkIndex(newIndex);
+    });
+  };
+  //   prevbtn
+  const prevTestimony = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkIndex(newIndex);
+    });
+  };
+  //   random testimony
+  const randomTestimony = () => {
+    let randomIndex = Math.floor(Math.random() * testimonies.length);
+    if (randomIndex === index) {
+      randomIndex = index + 1;
+    }
+    return setIndex(checkIndex(randomIndex));
+  };
+  return (
+    <main>
+      <div className="review container">
+        <section>
+          <figure className="img-container">
+            <img src={image} alt={name} className="person-img" />
+          </figure>
+          <article>
+            <p className="info">
+              <FaQuoteLeft /> <span>{text}</span> <FaQuoteRight />
+            </p>
+            <div className="details">
+              <h3 className="author">{name}</h3>
+              <h4 className="job">{job}</h4>
+            </div>
+            <div className="yes">
+              <button className="prev-btn" onClick={prevTestimony}>
+                <FaChevronLeft />
+              </button>
+              <button className="next-btn" onClick={nextTestimony}>
+                <FaChevronRight />
+              </button>
+            </div>
+            <button className="random-btn" onClick={randomTestimony}>
+              random
+            </button>
+          </article>
+        </section>
+      </div>
+    </main>
+  );
+}
